2630d114e782f939f0d4cfceb1345555fff7046a
Justin Taylor <ju.tay.lor2019@gmail.com>
2022-08-16 14:10:45 -0400

    intial

---
+++ Public/js/api.js
@@ -0,0 +1,4 @@
+let api_key = "3ed01b89c3b424928120b1e5a535431d";
+let imgUrl = "https://image.tmdb.org/t/p/w500/";
+let genresListHttp = "https://api.themoviedb.org/3/genre/movie/list?"
+let moviesDataHttp = "https://api.themoviedb.org/3/discover/movie?"
