d53e3c31f299d3d88a5c9e99bb81f2ae77a9e6e6
Justin Taylor <ju.tay.lor2019@gmail.com>
2022-09-07 23:28:23 -0400

    visually complete

--- Public/js/home.js
+++ Public/js/home.js
@@ -1,58 +1,125 @@
-const main = document.querySelector('main');
+// const main = document.querySelector('main');
+// fetch(genresListHttp + new URLSearchParams({
+//   api_key: api_key
+// }))
+// .then((res) =>
+// {return res.json()})
+// .then((data) => {
+//   data.genres.forEach(item => {
+//     fetchMoviesListByGenres(item.id, item.name);
+//   })
+// })
+// const fetchMoviesListByGenres = (id,genres) => {
+//   fetch(moviesDataHttp + new URLSearchParams({
+//     api_key : api_key,
+//     with_genres: id,
+//   }))
+//   .then(res => res.json())
+//   .then(data => {
+//     makeCategoryElement(`${genres}_movies`, data.results);
+//   })
+//   .catch(err => console.log(err))
+// }
+// const makeCategoryElement = (category, data) => {
+//
+//
+//   document.main.innerHTML += `<div class="movie-list">
+//
+//     <button class="pre-btn"><i class="fa-solid fa-angles-left"></i></button>
+//
+//     <h1 class="movie-category">${category.split("_").join(" ")}</h1>
+//
+//     <div class="movie-container" id="${category}">
+//
+//     </div>
+//
+//     <button class="nxt-btn"><i class="fa-solid fa-angles-right"></i></button>
+//
+//   </div>`;
+//
+//   makeCards(category, data);
+//
+//   const makeCard = (id, data) => {
+//     const movieContainer = document.getElementById(id);
+//     data.forEach((item, i) => {
+//       if(item.backdrop_path == null){
+//         item.backdrop_path == item.poster_path;
+//         if(item.backdrop_path == null){
+//           return "i love duchess";
+//         }
+//       }
+//       movieContainer.innerHTML += `
+//       <div class="movie">
+//         <img src="${imgURL}${item.backdrop_path}" alt="" />
+//         <p class="movie-title">${item.title}</p>
+//       </div>
+//       `
+//     })
+//   }
+// }
+
+
+// Web Imported Code
+
+const main = document.querySelector('.main');
+
 fetch(genresListHttp + new URLSearchParams({
-  api_key: api_key
+    api_key: api_key
 }))
 .then(res => res.json())
 .then(data => {
-  data.genres.forEach(item => {
-    fetchMoviesListByGenres(item.id, item.name);
-  })
-})
-const fetchMoviesListByGenres = (id,genres) => {
-  fetch(moviesDataHttp + new URLSearchParams({
-    api_key : api_key,
-    with_genres: id, 
-  }))
-  .then(res => res.json())
-  .then(data => {
-    makeCategoryElement(`${genres}_movies`, data.results);
-  })
-  .catch(err => console.log(err))
+    data.genres.forEach(item => {
+        fetchMoviesListByGenres(item.id, item.name);
+    })
+});
+
+const fetchMoviesListByGenres = (id, genres) => {
+    fetch(moviesDataHttp + new URLSearchParams({
+        api_key: api_key,
+        with_genres: id,
+        page: Math.floor(Math.random() * 3) + 1
+    }))
+    .then(res => res.json())
+    .then(data => {
+        makeCategoryElement(`${genres}_movies`, data.results);
+    })
+    .catch(err =>  console.log(err));
 }
+
 const makeCategoryElement = (category, data) => {
-
-
-  document.main.innerHTML += `<div class="movie-list">
-
-    <button class="pre-btn"><i class="fa-solid fa-angles-left"></i></button>
-
-    <h1 class="movie-category">${category.split("_").join(" ")}</h1>
-
-    <div class="movie-container" id="${category}">
-
+    main.innerHTML += `
+    <div class="movie-list">
+        <button class="pre-btn"><i class="fa-solid fa-angles-left"></i></button>
+        <h1 class="movie-category">${category.split("_").join(" ")}</h1>
+        <div class="movie-container" id="${category}">
+        </div>
+        <button class="nxt-btn"><i class="fa-solid fa-angles-right"></i></button>
     </div>
+    `;
+    makeCards(category, data);
+}
 
-    <button class="nxt-btn"><i class="fa-solid fa-angles-right"></i></button>
-
-  </div>`;
-
-  makeCards(category, data);
-
-  const makeCard = (id, data) => {
+const makeCards = (id, data) => {
     const movieContainer = document.getElementById(id);
     data.forEach((item, i) => {
-      if(item.backdrop_path == null){
-        item.backdrop_path == item.poster_path;
         if(item.backdrop_path == null){
-          return;
+            item.backdrop_path = item.poster_path;
+            if(item.backdrop_path == null){
+                return;
+            }
         }
-      }
-      movieContainer.innerHTML += `
-      <div class="movie">
-        <img src="${imgURL}${item.backdrop_path}" alt="" />
-        <p class="movie-title">${item.title}</p>
-      </div>
-      `
+
+        movieContainer.innerHTML += `
+        <div class="movie" onclick="location.href = '/moviePage.html'">
+            <img src="${imgUrl}${item.backdrop_path}" alt="">
+            <p class="movie-title">${item.title}</p>
+        </div>
+        `;
+
+        if(i == data.length - 1){
+            setTimeout(() => {
+                setupScrolling();
+            }, 100);
+        }
     })
-  }
 }
