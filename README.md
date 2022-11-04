d24dce7adc77c7c5c03d54f190f4e15810581ac3
Justin Taylor <ju.tay.lor2019@gmail.com>
2022-11-04 14:18:10 -0400

    added the app

---
+++ README.md
@@ -0,0 +1,4 @@
+# Netflix-Clone
+<p>This is a frontend remake of a single Netflix page. This was made using React and html/css.</p>
+<br>
+<p>This project is one of several projects that i've made to display html/css capabilties so functionalitlies such as authorization and authentication systems have not been implemented due to relevance.
---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+let intial_path = path.join(__dirname, "Public")
+
+let app = express();
+app.use(express.static(intial_path));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(intial_path, "index.html"))
+})
+
+app.listen(3000, () => {
+  console.log('listening on port 3000......');
+})
